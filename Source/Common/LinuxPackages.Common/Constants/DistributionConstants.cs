acd059f8888c82371f02f9615920ed2a755a404e
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2016-02-05 14:44:02 +0200

    Introduced Distribution entity and implemented service for it

---
+++ Source/Common/LinuxPackages.Common/Constants/DistributionConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace LinuxPackages.Common.Constants
+{
+    public class DistributionConstants
+    {
+        public const int MinDistributionName = 3;
+        public const int MaxDistributionName = 50;
+
+        public const int MinVersionLength = 1;
+        public const int MaxVersionLength = 15;
+
+        public const int MinMaintainerLength = 3;
+        public const int MaxMaintainerLength = 50;
+    }
+}
---
+++ Source/Services/LinuxPackages.Services.Data/Contracts/IDistributionsService.cs
@@ -0,0 +1,14 @@
+﻿namespace LinuxPackages.Services.Data.Contracts
+{
+    using LinuxPackages.Data.Models;
+    using System.Linq;
+
+    public interface IDistributionsService
+    {
+        IQueryable<Distribution> GetAll();
+
+        IQueryable<Distribution> GetById(int id);
+
+        Distribution Create(string name, string maintainer, string url);
+    }
+}
