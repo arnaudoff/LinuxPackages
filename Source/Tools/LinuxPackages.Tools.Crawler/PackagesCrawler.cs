7d3971cd88d7474be8859c3305474d17021562f9
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2016-02-12 22:56:48 +0200

    Integrated AngleSharp and implemented the crawler

---
+++ Source/Tools/LinuxPackages.Tools.Crawler/PackagesCrawler.cs
@@ -0,0 +1,113 @@
+﻿namespace LinuxPackages.Tools.Crawler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using System.Threading;
+
+    using AngleSharp;
+    using AngleSharp.Dom.Html;
+
+    using Models;
+    using Contracts;
+
+    public class PackagesCrawler : ICrawler
+    {
+        private const string Url = "https://packages.debian.org/stable/";
+        private const int NewCategoryRequestDelay = 10000;
+
+        private readonly IConfiguration configuration;
+        private readonly IBrowsingContext browsingContext;
+        private readonly IPackagesExporter exporter;
+
+        public PackagesCrawler()
+            : this(new CsvFilePackagesExporter())
+        {
+        }
+
+        public PackagesCrawler(IPackagesExporter exporter)
+        {
+            this.configuration = Configuration.Default.WithDefaultLoader();
+            this.browsingContext = BrowsingContext.New(configuration);
+            this.exporter = exporter;
+        }
+
+        public void Crawl()
+        {
+            IEnumerable<Category> categories = this.ParseCategories();
+            IEnumerable<Package> packages = this.ParsePackages(categories);
+            exporter.Export(packages);
+        }
+
+        private IList<Package> ParsePackages(IEnumerable<Category> categories)
+        {
+            var packages = new List<Package>();
+
+            foreach (var category in categories)
+            {
+                var currentPackageDocument = this.browsingContext.OpenAsync(category.Url).Result;
+                var packageElements = currentPackageDocument.QuerySelectorAll("#content dl dt a");
+                var packageDescription = currentPackageDocument.QuerySelectorAll("#content dl dd");
+
+                for (int i = 0; i < packageElements.Length; i++)
+                {
+                    var currentPackage = new Package()
+                    {
+                        Name = packageElements[i].TextContent,
+                        Description = packageDescription[i].TextContent,
+                        CategoryName = category.Name
+                    };
+
+                    packages.Add(currentPackage);
+                }
+
+                Console.WriteLine($"Crawled category {category.Name}. ({NewCategoryRequestDelay} ms timeout..)");
+                Thread.Sleep(NewCategoryRequestDelay);
+            }
+
+            return packages;
+        }
+
+        private IList<Category> ParseCategories()
+        {
+            var categoriesDocument = this.browsingContext.OpenAsync(Url).Result;
+
+            var leftPackagesCategories = categoriesDocument.QuerySelectorAll("#lefthalfcol dl dt");
+            var leftPackagesCategoriesDescriptions = categoriesDocument.QuerySelectorAll("#lefthalfcol dl dd");
+
+            var rightPackagesCategories = categoriesDocument.QuerySelectorAll("#righthalfcol dl dt");
+            var rightPackagesCategoriesDescriptions = categoriesDocument.QuerySelectorAll("#righthalfcol dl dd");
+
+            var categories = new List<Category>();
+
+            for (int i = 0; i < leftPackagesCategories.Length; i++)
+            {
+                var categoryAnchor = (IHtmlAnchorElement)leftPackagesCategories[i].FirstChild;
+                var currentCategory = new Category()
+                {
+                    Name = categoryAnchor.Text,
+                    Description = leftPackagesCategoriesDescriptions[i].TextContent,
+                    Url = categoryAnchor.Href
+                };
+
+                categories.Add(currentCategory);
+            }
+
+            for (int i = 0; i < rightPackagesCategories.Length; i++)
+            {
+                var categoryAnchor = (IHtmlAnchorElement)rightPackagesCategories[i].FirstChild;
+                var currentCategory = new Category()
+                {
+                    Name = categoryAnchor.Text,
+                    Description = rightPackagesCategoriesDescriptions[i].TextContent,
+                    Url = categoryAnchor.Href
+                };
+
+                categories.Add(currentCategory);
+            }
+
+            return categories;
+        }
+    }
+}
---
+++ Source/Tools/LinuxPackages.Tools.Crawler/Startup.cs
@@ -0,0 +1,11 @@
+﻿namespace LinuxPackages.Tools.Crawler
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var packagesCrawler = new PackagesCrawler();
+            packagesCrawler.Crawl();
+        }
+    }
+}
