416d1600882cacbb2eba301358488be012e14419
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2016-02-21 02:34:57 +0200

    Implemented view models for the admin index page

---
+++ Source/Web/LinuxPackages.Web.Mvc/Areas/Admin/ViewModels/Home/ListedLatestCommentViewModel.cs
@@ -0,0 +1,46 @@
+﻿namespace LinuxPackages.Web.Mvc.Areas.Admin.ViewModels
+{
+    using System;
+    using AutoMapper;
+    using Data.Models;
+    using LinuxPackages.Web.Mvc.Infrastructure.Mappings;
+    using Infrastructure.Helpers;
+
+    public class ListedLatestCommentViewModel : IMapFrom<PackageComment>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public string ShortenedContent
+        {
+            get
+            {
+                return this.Content.Length > 60 ? this.Content.Substring(0, 60) + "..." : this.Content;
+            }
+        }
+
+        public DateTime CreatedOn { get; set; }
+
+        public int PackageId { get; set; }
+
+        public string PackageUrl
+        {
+            get
+            {
+                return new UrlIdentifierProvider().EncodeEntityId(this.PackageId);
+            }
+        }
+
+        public string PackageName { get; set; }
+
+        public string AuthorUsername { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<PackageComment, ListedLatestCommentViewModel>()
+                .ForMember(c => c.PackageName, opts => opts.MapFrom(c => c.Package.Name))
+                .ForMember(c => c.AuthorUsername, opts => opts.MapFrom(c => c.Author.UserName));
+        }
+    }
+}
---
+++ Source/Web/LinuxPackages.Web.Mvc/Areas/Admin/ViewModels/Home/ListedLatestIssueReplyViewModel.cs
@@ -0,0 +1,43 @@
+﻿namespace LinuxPackages.Web.Mvc.Areas.Admin.ViewModels
+{
+    using System;
+    using AutoMapper;
+    using Infrastructure.Helpers;
+    using LinuxPackages.Data.Models;
+    using LinuxPackages.Web.Mvc.Infrastructure.Mappings;
+
+    public class ListedLatestIssueReplyViewModel : IMapFrom<IssueReply>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public string ShortenedContent
+        {
+            get
+            {
+                return this.Content.Length > 60 ? this.Content.Substring(0, 60) + "..." : this.Content;
+            }
+        }
+
+        public DateTime CreatedOn { get; set; }
+
+        public int IssueId { get; set; }
+
+        public string IssueUrl
+        {
+            get
+            {
+                return new UrlIdentifierProvider().EncodeEntityId(this.IssueId);
+            }
+        }
+
+        public string AuthorUsername { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<IssueReply, ListedLatestIssueReplyViewModel>()
+                .ForMember(r => r.AuthorUsername, opts => opts.MapFrom(r => r.Author.UserName));
+        }
+    }
+}
