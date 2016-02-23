2f6fb657168bc33c3a80f4bb81e39286cefffb0f
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2016-02-23 12:49:15 +0200

    Fixed minor bugs on the home page and issue details

--- Source/Web/LinuxPackages.Web.Mvc/ViewModels/Issues/ListedRecentIssuesViewModel.cs
+++ Source/Web/LinuxPackages.Web.Mvc/ViewModels/Issues/ListedRecentIssuesViewModel.cs
@@ -1,8 +1,8 @@
 ﻿namespace LinuxPackages.Web.Mvc.ViewModels.Issues
 {
-    using LinuxPackages.Data.Models;
-    using LinuxPackages.Web.Mvc.Infrastructure.Helpers;
-    using LinuxPackages.Web.Mvc.Infrastructure.Mappings;
+    using Data.Models;
+    using Infrastructure.Helpers;
+    using Infrastructure.Mappings;
     using Packages;
 
     public class ListedRecentIssuesViewModel : IMapFrom<Issue>
@@ -13,12 +13,40 @@
         {
             get
             {
-                return (new UrlIdentifierProvider()).EncodeEntityId(this.Id);
+                return new UrlIdentifierProvider().EncodeEntityId(this.Id);
             }
         }
 
         public string Title { get; set; }
 
-        public ListedPackageViewModel Package { get; set; }
+        public string ShortenedTitle
+        {
+            get
+            {
+                if (this.Title != null)
+                {
+                    return this.Title.Length > 15 ? this.Title.Substring(0, 15) + "..." : this.Title;
+                }
+
+                return null;
+            }
+        }
+
+        public int PackageId { get; set; }
+
+        public string PackageName { get; set; }
+
+        public string ShortenedPackageName
+        {
+            get
+            {
+                if (this.PackageName != null)
+                {
+                    return this.PackageName.Length > 15 ? this.PackageName.Substring(0, 15) + "..." : this.PackageName;
+                }
+
+                return null;
+            }
+        }
     }
 }
